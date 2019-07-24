288d9e8e66b7ac2f4672711cb3942e2f5c0f88a0
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-07-24 14:18:22 -0400

    finished urlify

--- drills.js
+++ drills.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function urlify(string){
-  
+  let result = '';
+  if (string.includes(' ')) {
+    result = string.replace(/ /g, '%20');
+    return result;
+  } else {
+    return string;
+  }
 }
 
-console.log(urlify('tauhida parveen'));
+// console.log(urlify('tauhida parveen with more spaces'));
+
